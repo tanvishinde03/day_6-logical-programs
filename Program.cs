b2b569aa7cf49e45d0c7530dc92831db7f02b518
tanvishinde03 <shindetanvi0203@gmail.com>
2022-12-29 16:49:21 +0530

    first

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using logicalproblems;
+using LogicalPrograms;
+using logicprograms;
+
+namespace logicprograms
+{
+    public class program
+    {
+        public static void Main(String[] args)
+        {
+            fibonacci.Displayfibonacci();
+            perfectnumber.checkperfectnumber();
+            PrimeNumbers.Prime();
+            reversenumber.Display();
+            coupanNumber.Coupon();
+            timeElapsed.stopwatch();
+        }
+    }
+}
