b1e87eb3cf1defb5bb0d16e0fe84f5793c8331fb
tanvishinde03 <shindetanvi0203@gmail.com>
2022-12-29 17:21:13 +0530

    UC2 perfectnumber

---
+++ perfectnumber.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    
+
+namespace logicprograms
+    {
+        internal class perfectnumber
+        {
+            public static void checkperfectnumber()
+            {
+                int n, i, sum;
+                int mn, mx;
+                Console.WriteLine("find the perfect numbers within a given number of set");
+                Console.WriteLine("Input the starting range of number ");
+                mn = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Input the ending range of number ");
+                mx = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("To perfect numbers withing the given range :");
+                for (n = mn; n < mx; n++)
+                {
+                    i = 1;
+                    sum = 0;
+                    while (i < n)
+                    {
+                        if (n % i == 0)
+                            sum = sum + i;
+                        i++;
+                    }
+                    if (sum == n)
+                        Console.Write("{0} ", n);
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
