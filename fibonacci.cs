2b96c8b8f12e9922b0c22674cf4f519c6cb83786
tanvishinde03 <shindetanvi0203@gmail.com>
2022-12-29 17:17:04 +0530

    UC1 fibonacci

---
+++ fibonacci.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace logicprograms
+    {
+        public class fibonacci
+        {
+            public static void Displayfibonacci()
+            {
+                int n1 = 0, n2 = 1, n3, i, number;
+                Console.WriteLine("Enter the number of elements");
+                number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(n1 + " " + n2 + " ");
+
+                for (i = 2; i < number; i++)
+                {
+                    n3 = n1 + n2;
+                    Console.Write(n3 + " ");
+                    n1 = n2;
+                    n2 = n3;
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
