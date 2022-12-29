1152deed4a1a037b0c3465808f9b6a6a60e794ea
tanvishinde03 <shindetanvi0203@gmail.com>
2022-12-29 17:39:01 +0530

    UC6 stopwatch.cs

---
+++ stopwatch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+    {
+        internal class timeElapsed
+        {
+            public static void stopwatch()
+            {
+
+                Stopwatch stopwatch = new Stopwatch();
+
+
+                stopwatch.Start();
+
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Thread.Sleep(10);
+                }
+
+
+                stopwatch.Stop();
+
+
+                Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+                 Console.ReadLine();
+            }
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
