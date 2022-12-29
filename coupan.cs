30af0567ad23ca012bc99f879a9e7eba36e9d522
tanvishinde03 <shindetanvi0203@gmail.com>
2022-12-29 17:33:47 +0530

    UC5 coupan.cs

---
+++ coupan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicalproblems
+{
+    public  class coupanNumber
+        {
+            public static void Coupon()
+            {
+                var numbers = "0123456789ABCDEFGHIJKLMNOPQRST";
+                var array = new char[6];
+                var random = new Random();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = numbers[random.Next(numbers.Length)];
+                }
+                var finalString = new String(array);
+                Console.WriteLine("The distinct coupon number is: " + finalString);
+                Console.ReadLine();
+            }
+        }
+
+    }
+    
+
